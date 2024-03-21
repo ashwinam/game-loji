17f66af06a716b484c0546a594b8548453b54648
ashwinPy <ashwinam31@gmail.com>
2024-03-21 10:50:08 +0530

    Shadcn Ui Lib configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="h-screen w-full text-3xl font-bold underline bg-zinc-800 text-white">
+      Hello Ashwin!
+    </h1>
+  );
 }
 
 export default App;
