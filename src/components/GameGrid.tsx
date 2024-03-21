62d1fbc4d9f0429c0f617c35bf7ca39572af4411
ashwinPy <ashwinam31@gmail.com>
2024-03-21 16:33:35 +0530

    Created a custom hook for fetching games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,21 +1,7 @@
-import { useEffect, useState } from "react";
-
-import ApiClient from "@/services/ApiClient";
-
-interface Game {
-  id: number;
-  name: string;
-}
+import useGames from "@/hooks/useGames";
 
 function GameGrid() {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    ApiClient.get("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
