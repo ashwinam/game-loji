8b1a37b047c7f10d31312ec4a5ecc84026aa2140
ashwinPy <ashwinam31@gmail.com>
2024-03-21 13:02:01 +0530

    update: navbar configured base mode toggler

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import light_theme_logo from "./assets/removed_bg_logo_cleanup.png";
+import dark_theme_logo from "./assets/removed_bg_logo.png";
+import { ModeToggle } from "./components/mode-toggle";
+import { useTheme } from "./components/theme-provider";
+
 function App() {
   return (
-    <div className="grid m-5 sm:grid-cols-2 gap-4">
-      <div className="nav sm:col-span-2 bg-sky-500 h-20 shadow-lg">Nav</div>
+    <div className="grid sm:grid-cols-2 gap-4">
+      <div className="nav sm:col-span-2 flex items-center justify-between pr-4">
+        <img
+          src={
+            useTheme().theme === "light" ? light_theme_logo : dark_theme_logo
+          }
+          alt=""
+          className="w-24"
+        />
+        <ModeToggle />
+      </div>
       <div className="aside bg-orange-500 hidden sm:block h-20 shadow-lg">
         aside
       </div>
