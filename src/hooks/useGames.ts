12127ce8c9228b94b7c7fcdc9fa3b2ca1de68b2c
ashwinPy <ashwinam31@gmail.com>
2024-03-22 18:15:42 +0530

    update: enhance user experience by adding skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -20,16 +20,21 @@ export interface Game {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(true);
   
     useEffect(() => {
 
         const controller = new AbortController();
 
       ApiClient.get("/games", {signal: controller.signal})
-        .then((res) => setGames(res.data.results))
+        .then((res) => {
+          setGames(res.data.results)
+          setLoading(false);
+        })
         .catch((err) => {
             if (err instanceof CanceledError) return;
             setError(err.message)
+            setLoading(false);
         });
 
         return () => controller.abort();
@@ -37,7 +42,7 @@ const useGames = () => {
     }, []);
 
 
-    return {games, error};
+    return {games, error, isLoading};
 }
 
 export default useGames;
