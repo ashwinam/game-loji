3efdc10db6d1d5c25f571d34e2b1bec99a40de99
ashwinPy <ashwinam31@gmail.com>
2024-03-27 16:33:02 +0530

    update: title changed

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -28,7 +28,7 @@ function GameGrid({
   if (error) return <p>{error}</p>;
 
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-3  gap-6">
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 sm:p-3">
       {isLoading &&
         skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
 
